06f2fa09af3c76ce495ce6209bf7dafaadf53954
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-11-05 02:12:41 +0530

    Update helpers.py

--- Crush/helpers.py
+++ Crush/helpers.py
@@ -76,36 +76,19 @@ asyncio.create_task(auto_clean())
 
 
 RANDOM = [
-    "https://telegra.ph//file/0879fbdb307005c1fa8ab.jpg",
-    "https://telegra.ph//file/19e3a9d5c0985702497fb.jpg",
-    "https://telegra.ph//file/b5fa277081dddbddd0b12.jpg",
-    "https://telegra.ph//file/96e96245fe1afb82d0398.jpg",
-    "https://telegra.ph//file/fb140807129a3ccb60164.jpg",
-    "https://telegra.ph//file/09c9ea0e2660efae6f62a.jpg",
-    "https://telegra.ph//file/3b59b15e1914b4fa18b71.jpg",
-    "https://telegra.ph//file/efb26cc17eef6fe82d910.jpg",
-    "https://telegra.ph//file/ab4925a050e07b00f63c5.jpg",
-    "https://telegra.ph//file/d169a77fd52b46e421414.jpg",
-    "https://telegra.ph//file/dab9fc41f214f9cded1bb.jpg",
-    "https://telegra.ph//file/e05d6e4faff7497c5ae56.jpg",
-    "https://telegra.ph//file/1e54f0fff666dd53da66f.jpg",
-    "https://telegra.ph//file/18e98c60b253d4d926f5f.jpg",
-    "https://telegra.ph//file/b1f7d9702f8ea590b2e0c.jpg",
-    "https://telegra.ph//file/7bb62c8a0f399f6ee1f33.jpg",
-    "https://telegra.ph//file/dd00c759805082830b6b6.jpg",
-    "https://telegra.ph//file/3b996e3241cf93d102adc.jpg",
-    "https://telegra.ph//file/610cc4522c7d0f69e1eb8.jpg",
-    "https://telegra.ph//file/bc97b1e9bbe6d6db36984.jpg",
-    "https://telegra.ph//file/2ddf3521636d4b17df6dd.jpg",
-    "https://telegra.ph//file/72e4414f618111ea90a57.jpg",
-    "https://telegra.ph//file/a958417dcd966d341bfe2.jpg",
-    "https://telegra.ph//file/0afd9c2f70c6328a1e53a.jpg",
-    "https://telegra.ph//file/82ff887aad046c3bcc9a3.jpg",
-    "https://telegra.ph//file/8ba64d5506c23acb67ff4.jpg",
-    "https://telegra.ph//file/8ba64d5506c23acb67ff4.jpg",
-    "https://telegra.ph//file/a7cba6e78bb63e1b4aefb.jpg",
-    "https://telegra.ph//file/f8ba75bdbb9931cbc8229.jpg",
-    "https://telegra.ph//file/07bb5f805178ec24871d3.jpg"
+    "https://te.legra.ph/file/726926b642c34831ea787.jpg"
+    "https://te.legra.ph/file/945b6fd999a4774d9e8f5.jpg""
+    "https://te.legra.ph/file/55525404e91234ab6dda5.jpg"
+    "https://te.legra.ph/file/cef41d4ec3e0bb822e98c.jpg"
+    "https://te.legra.ph/file/8cec6df3f12328d3bd3bf.jpg"
+    "https://te.legra.ph/file/f58a778b4dd5451c7f4cc.jpg"
+    "https://te.legra.ph/file/6465ee4a422093c2208e6.jpg"
+    "https://te.legra.ph/file/f4259552c1a85cab0edf8.jpg"
+    "https://te.legra.ph/file/4c09105659848dd51a1be.jpg"
+    "https://te.legra.ph/file/689d7577c3c478d7a4ef4.jpg"
+    "https://te.legra.ph/file/e4ee341d973bc6ece2374.jpg"
+    "https://te.legra.ph/file/97173c58396675272802b.jpg"
+    "https://te.legra.ph/file/441ad92db4fa010f3db36.jpg"
 ]
 
 
@@ -124,6 +107,8 @@ HELP_TEXT = f"""Welcome to {botname}'s Help Section.
 
 
 /settings - To change or edit basic settings of AFK Bot.
+
+For Any Help :- @crushbot_support
 """
 
 def settings_markup(status: Union[bool, str] = None):

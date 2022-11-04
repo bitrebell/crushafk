80bd20ac1d8b664e69e5a34bcab190f503cb6f9f
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-11-05 00:21:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 #
 # This file is part of < https://github.com/Darkranger00/crushafk > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/Darkranger00/crushafk/tree/main/LICENSE >
+# Please see < https://github.com/Darkranger00/crushafk/blob/main/LICENSE >
 #
 # All rights reserved.
 #

eea7cbedea9f1a452512fb56a2b2e49424753b8c
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2023-03-03 23:37:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,16 +15,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID =  10738943
+API_HASH = "da61e3a08b5ac78ce28b4a4cd854aeec"
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = "6151806723:AAEOkTGYlhQNPjrUHZAZBUSnIv-qDnTv-LI"
 
 # Database to save your chats and stats...
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = "mongodb+srv://adil00:12345@cluster0.fyt5yoa.mongodb.net/?retryWrites=true&w=majority"
 
 # SUDO USERS
-SUDO_USER = list(
-    map(int, getenv("SUDO_USER", "").split())
-)  # Input type must be interger
+SUDO_USER = "5716978554"
+# Input type must be interger

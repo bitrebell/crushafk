048be3b5251497122415386e498b4f96557fba87
AADIL SHIEKH <114225192+bitrebell@users.noreply.github.com>
2023-09-16 10:51:42 +0530

    just upgrade

--- Crush/modules/start.py
+++ Crush/modules/start.py
@@ -40,7 +40,7 @@ async def on_start(_, message: Message):
         ]
     )
     image = random.choice(RANDOM)
-    send = await message.reply_photo(image, caption=f"Hello! My name is {botname}.\n\nI am designed for AFK by [𝗕𝗶𝘁𝗥𝗘𝗯𝗲𝗹𝗹،(💛](https://t.me/aadillllll)\nTo know more about me check help section.Active since {Uptime}", reply_markup=upl)
+    send = await message.reply_photo(image, caption=f"Hello! My name is {botname}.\n\nI am designed for AFK by [𝗕𝗶𝘁𝗥𝗘𝗯𝗲𝗹𝗹🤍](https://t.me/aadillllll)\nTo know more about me check help section.Active since {Uptime}", reply_markup=upl)
     await put_cleanmode(message.chat.id, send.message_id)
     
 

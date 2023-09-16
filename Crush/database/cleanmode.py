ff971eb21ac3b1b89c7416de8b31d0b87b6f8296
AADIL SHIEKH <114225192+bitrebell@users.noreply.github.com>
2023-09-16 10:38:41 +0530

    upgrade to a version 1.0.1

--- Crush/database/cleanmode.py
+++ Crush/database/cleanmode.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2022-2023 by @Darkranger00, < https://github.com/Darkranger00 >.
+# Copyright (C) 2022-2023 by @bitrebell, < https://github.com/birebell >.
 #
-# This file is part of < https://github.com/Darkranger00/crushafk > project,
+# This file is part of < https://github.com/bitrebell/crushafk > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/Darkranger00/crushafk/tree/main/LICENSE >
+# Please see < https://github.com/Darkranger00/bitrebell/tree/main/LICENSE >
 #
 # All rights reserved.
 #
